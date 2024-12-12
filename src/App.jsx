16fdf232c6a161af1710b5c5b1ca0cb636e27a3c
Benjamin <goosta19802@gmail.com>
2024-12-12 17:25:46 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
         <About />
         <Parties />
         <Team />
-        <Feedbacks />
+        {/* <Feedbacks /> */}
         <ContactButton />
         <div className="relative z-0">
           {/* <Contact /> */}
