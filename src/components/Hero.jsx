1769c391a0b92bb5bd212b8fb01c8e643e4e539f
BenjaminOrellanaDev <goosta19802@gmail.com>
2024-12-13 11:26:01 -0300

    Cambio en imagen hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import './span.css';
 const Hero = () => {
   return (
     <section
-      className="relative w-full h-screen sm:h-[100vh] mx-auto bg-bar-bg bg-no-repeat bg-cover bg-fixed bg-center"
+      className="relative w-full h-screen sm:h-[100vh] mx-auto bg-bar-bg bg-no-repeat bg-cover bg-center sm:bg-fixed"
       id="home"
     >
       <div
