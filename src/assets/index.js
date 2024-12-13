a013824c96060d169747ce41cf9af9d1fd67f76f
BenjaminOrellanaDev <goosta19802@gmail.com>
2024-12-13 11:03:14 -0300

    Cambios 3

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,12 +1,8 @@
 import logo from './logo.svg';
-import backend from './backend.png';
-import web from './web.png';
-import github from './github.png';
 import menu from './menu.svg';
 import close from './close.svg';
 import user from './user.svg';
 
-import android from './android.jpeg';
 import estrella from './estrella.svg';
 
-export { logo, backend, web, github, menu, close, user, android, estrella };
+export { logo, menu, close, user, estrella };
